2806455a112d235d51541ca900e0535680677e12
shiotomo <t.shiota16@gmail.com>
2016-10-14 20:07:08 +0900

    IMplements mute sound when pushed touch sensor.

---
+++ TouchSensor01_2/src/Main.java
@@ -0,0 +1,14 @@
+import jp.ac.kagawa_u.infoexpr.Sensor.TouchSensor;
+import lejos.hardware.Sound;
+import lejos.hardware.port.SensorPort;
+
+public class Main {
+ 
+    private static TouchSensor touch = new TouchSensor(SensorPort.S1);    
+ 
+    public static void main(String[] args) {
+        while ( ! touch.isPressed() ) {
+            Sound.beep();
+        }
+    }
+}
